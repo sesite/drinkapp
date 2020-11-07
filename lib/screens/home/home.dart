ede64e0197c118d626f8a136639cb9cac026560b
SVA <mr.supervit@gmail.com>
2020-11-07 17:03:28 +0300

    complete

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -4,16 +4,20 @@ import 'package:drinkapp/screens/home/settingsform.dart';
 import 'package:drinkapp/services/auth.dart';
 import 'package:drinkapp/services/database.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 class Home extends StatelessWidget {
   //const Home({Key key}) : super(key: key);
 
   final AuthService _auth = AuthService();
-  final DatabaseService _db = DatabaseService();
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     void _showSettingsPanel() {
       showModalBottomSheet(
           context: context,
